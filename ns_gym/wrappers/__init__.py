370d8cd25b80865bac05ed6c8046444f57bc9829
nkepling <nathankep@gmail.com>
2025-07-28 11:49:20 -0500

    init file

--- ns_gym/wrappers/__init__.py
+++ ns_gym/wrappers/__init__.py
@@ -1,3 +1,4 @@
 #from .box_2d import *
 from .classic_control import NSClassicControlWrapper
 from .toy_text import NSBridgeWrapper,NSCliffWalkingWrapper,NSFrozenLakeWrapper
+from .mujoco_env import MujocoWrapper
